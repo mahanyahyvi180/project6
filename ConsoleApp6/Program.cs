d077281b29aab6bb0e99c5a436f28c8d4ce8a259
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-07-29 16:59:56 +0330

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] gradesArray = { 87, 68, 94, 100, 83, 78, 85, 91, 76, 87 };
+
+            var myGradeBook = new GradeBook("CS101 Introduction to c# programing", gradesArray);
+
+            myGradeBook.DisplayMessage();
+            myGradeBook.processGrades();
+
+            Console.ReadLine();
+        }
+    }
+}
